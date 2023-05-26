90000083b7764de577dd0987fcc1510b7ca81d71
PokarPetr <pokar.petr@gmail.com>
2023-05-26 09:17:24 +0200

    Added app.py with 'mock' functions

---
+++ app.py
@@ -0,0 +1,20 @@
+import requests as r
+from bs4 import BeautifulSoup
+
+def get_soup(url):
+    """
+    take a response from a site and build BeautifulSoup object
+    """
+    pass
+
+def get_fx_to_usd(currency):
+    """
+    get a rating between a currency of a ticker(depend on exchange where the ticker is traded) and USD
+    """
+    pass
+
+def get_price_information(ticker, exchange):
+    """
+    the main block to get a dictionary of values
+    """
+    pass
