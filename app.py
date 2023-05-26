8153a9ea263abf1b666de673eac03196fb24a743
PokarPetr <pokar.petr@gmail.com>
2023-05-26 09:47:22 +0200

    Added 2 functions (get_price_information() and get_value_in_usd()) to app.py

--- app.py
+++ app.py
@@ -10,17 +10,36 @@ def get_soup(url):
     return soup
 
 
-def get_fx_to_usd(currency):
+def get_value_in_usd(currency):
     """
     get a rating between a currency of a ticker(depend on exchange where the ticker is traded) and USD
+    exchange rate is taken from Google Finance
     """
-    pass
+    url = f'https://www.google.com/finance/quote/{currency}-USD'
+    soup = get_soup(url)
+    fx_rate = soup.find('div', attrs={'data-last-price': True})
+    fx = float(fx_rate['data-last-price'])
+    return fx
 
 def get_price_information(ticker, exchange):
     """
     the main block to get a dictionary of values
     """
-    pass
+    url = f'https://www.google.com/finance/quote/{ticker}:{exchange}'
+    soup = get_soup(url)
+
+    price_div = soup.find('div', attrs={'data-last-price': True})
+    price = float(price_div['data-last-price'])
+    currency = price_div['data-currency-code']
+    usd_price = price
+
+    if currency != 'USD':
+        fx = get_value_in_usd(currency)
+        usd_price = round(price * fx, 2)
+
+    return dict(ticker=ticker, exchange=exchange, price=price,
+                currency=currency, usd_price=usd_price)
+
 
 if __name__ == "__main__":
-    pass
+    print(get_price_information('GOOGL', 'NASDAQ'))
